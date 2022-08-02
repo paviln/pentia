e8a3855e3e1c4f00c611929f8572583f1c6a5a23
Jens Christensen <paviln@outlook.dk>
2022-08-02 21:17:46 +0200

    Implement social auth.

--- App.tsx
+++ App.tsx
@@ -1,121 +1,154 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {useEffect, type PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 import SplashScreen from 'react-native-splash-screen';
 import {
+  Alert,
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import config from './config';
+import {AccessToken, LoginManager} from 'react-native-fbsdk-next';
+import FacebookSigninButton from './src/modules/auth/components/FacebookSigninButton';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const PROFILE_IMAGE_SIZE = 150;
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  // Initializing state whilst app loads
+  const [initializing, setInitializing] = useState(true);
+  const [currentUser, setCurrentUser] =
+    useState<FirebaseAuthTypes.User | null>();
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  }, []);
+
+  useEffect(() => {
+    // Handle user state changes
+    const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+      setCurrentUser(user);
+      if (initializing) {
+        setInitializing(false);
+        SplashScreen.hide();
+      }
+    };
+
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+
+    return subscriber;
+  }, [initializing]);
+
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId: config.webClientId,
+      offlineAccess: false,
+      profileImageSize: PROFILE_IMAGE_SIZE,
+    });
+  };
+
+  //  Handle user sign in via facebook
+  const onFacebookButtonPress = async () => {
+    try {
+      // Attempt login with permissions
+      const result = await LoginManager.logInWithPermissions([
+        'public_profile',
+        'email',
+      ]);
+
+      if (result.isCancelled) {
+        throw new Error('Login with Facebook was cancelled');
+      }
+
+      // Once signed in, get the users AccesToken
+      const data = await AccessToken.getCurrentAccessToken();
+
+      if (!data) {
+        throw 'Something went wrong obtaining access token';
+      }
+
+      // Create a Firebase credential with the AccessToken
+      const facebookCredential = auth.FacebookAuthProvider.credential(
+        data.accessToken,
+      );
+
+      // Sign-in the user with the credential
+      auth().signInWithCredential(facebookCredential);
+    } catch (error) {
+      if (error instanceof Error) {
+        createDialog(error.message);
+      }
+    }
+  };
+
+  //  Handle user sign in via google
+  const onGoogleButtonPress = async () => {
+    try {
+      // Get the users ID token
+      const {idToken} = await GoogleSignin.signIn();
+
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      // Sign-in the user with the credential
+      await auth().signInWithCredential(googleCredential);
+    } catch (error) {
+      if (error instanceof Error) {
+        createDialog(error.message);
+      }
+    }
+  };
+
+  const createDialog = (message: string) =>
+    Alert.alert('Error', message, [
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!currentUser) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FacebookSigninButton onPress={() => onFacebookButtonPress()} />
+        <GoogleSigninButton
+          onPress={() =>
+            onGoogleButtonPress().then(() =>
+              console.log('Signed in with Google!'),
+            )
+          }
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>Welcome {currentUser.email}</Text>
+      <Button
+        title="Sign out"
+        onPress={() =>
+          auth()
+            .signOut()
+            .then(() => console.log('User signed out!'))
+        }
+      />
     </View>
   );
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
 });
 
---
+++ config.ts
@@ -0,0 +1,4 @@
+export default {
+  webClientId:
+    '609848145559-cflbli3gd5fokk1ktsgtt5k94engr3qa.apps.googleusercontent.com',
+};
